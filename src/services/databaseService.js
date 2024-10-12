61bf33fc01e43105ba0abb5df487da9fcf75c464
JoaoVictor-C <fffjv2006@gmail.com>
2024-10-11 22:22:50 -0300

    Update databaseService.js

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -6,11 +6,6 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            // Connection Pooling options
-            poolSize: 10, // Adjust based on your application's needs
-            serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-            family: 4 // Use IPv4, skip trying IPv6
         });
         console.log('✅ MongoDB Connected');
     } catch (error) {
