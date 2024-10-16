6e7daac07fd428cb509d21912c733970ddbd98f7
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2024-10-16 07:51:30 -0300

    Update GameOver.jsx

--- frontend/src/components/Game/GameOver.jsx
+++ frontend/src/components/Game/GameOver.jsx
@@ -1,11 +1,14 @@
 import '../../assets/styles/BlockChallengeHandler.css';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { playAgain } from '../../store/actions/gameActions';
 import PropTypes from 'prop-types';
+
 const GameOver = ({ gameId }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  
+  const winner = useSelector(state => state.game.currentGame.winner);
 
   const handlePlayAgain = () => {
     dispatch(playAgain(gameId))
@@ -17,15 +20,17 @@ const GameOver = ({ gameId }) => {
   const handleGoToHome = () => {
     navigate('/');
   };
+
   return (
     <div className="block-challenge-handler">
-        <div className="handler-content border border-light">
-            <h3 className="">The game is over</h3>
-            <div className="handler-buttons">
-                <button className="btn btn-primary" onClick={handlePlayAgain}>Play again</button>
-                <button className="btn btn-secondary" onClick={handleGoToHome}>Go to home</button>
-            </div>
+      <div className="handler-content border border-light">
+        <h3>The game is over</h3>
+        {winner && <p className="winner-text">Winner: {winner}</p>}
+        <div className="handler-buttons">
+          <button className="btn btn-primary" onClick={handlePlayAgain}>Play Again</button>
+          <button className="btn btn-secondary" onClick={handleGoToHome}>Go to Home</button>
         </div>
+      </div>
     </div>
   );
 };
