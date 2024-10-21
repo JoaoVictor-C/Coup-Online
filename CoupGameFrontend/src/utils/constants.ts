1b7f7bf13c292ca9fa7aa4c0961684b7072efa44
JoaoVictor-C <fffjv2006@gmail.com>
2024-10-21 12:26:39 -0300

    Updates

--- CoupGameFrontend/src/utils/constants.ts
+++ CoupGameFrontend/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const SIGNALR_HUB_URL = process.env.NODE_ENV === 'production'
   ? process.env.VITE_APP_SIGNALR_HUB_URL
-  : 'http://localhost:5000/gameHub';
+  : 'https://coup-online-941324057012.southamerica-east1.run.app/gameHub';
 
 export const API_BASE_URL = process.env.NODE_ENV === 'production'
   ? process.env.VITE_APP_API_BASE_URL
-  : 'http://localhost:5000/api';
+  : 'https://coup-online-941324057012.southamerica-east1.run.app/api';
