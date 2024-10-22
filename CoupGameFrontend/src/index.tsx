3599e6d757a3c0212bd4f095860a703ffa9128f2
JoaoVictor-C <fffjv2006@gmail.com>
2024-10-22 19:29:27 -0300

    Update index.tsx

--- CoupGameFrontend/src/index.tsx
+++ CoupGameFrontend/src/index.tsx
@@ -5,9 +5,6 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
