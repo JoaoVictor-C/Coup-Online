f9fbed969ecbc2868fdf844285cbc1b629d09600
João Victor <fffjv2006@gmail.com>
2024-10-22 14:30:29 -0300

    Atualizar o constants.ts

--- CoupGameFrontend/src/utils/constants.ts
+++ CoupGameFrontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const SIGNALR_HUB_URL = process.env.NODE_ENV === 'production'
   ? process.env.VITE_APP_SIGNALR_HUB_URL
-  : 'https://coup-online-941324057012.southamerica-east1.run.app/gamehub';
+  : 'https://coup-online-941324057012.southamerica-east1.run.app/gameHub';
 
 export const API_BASE_URL = process.env.NODE_ENV === 'production'
   ? process.env.VITE_APP_API_BASE_URL
