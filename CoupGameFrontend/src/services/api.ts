1bce20cd4dc68faa9ef34e57d2d2db48ed4b691b
João Victor <fffjv2006@gmail.com>
2024-10-22 14:38:11 -0300

    Atualizar o api.ts

--- CoupGameFrontend/src/services/api.ts
+++ CoupGameFrontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_BASE_URL } from '@utils/constants';
 
 const api = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: 'https://coup-online-941324057012.southamerica-east1.run.app/api'
 });
 
 // Add a request interceptor to include the token
