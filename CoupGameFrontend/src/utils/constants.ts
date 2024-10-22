b3d9d06dfc8e0b3601f6063eacc3293bcef76138
JoaoVictor-C <fffjv2006@gmail.com>
2024-10-22 19:49:09 -0300

    Fixes

--- CoupGameFrontend/src/utils/constants.ts
+++ CoupGameFrontend/src/utils/constants.ts
@@ -1,7 +1,9 @@
-export const SIGNALR_HUB_URL = import.meta.env.DEV
+console.log(import.meta.env.MODE);
+
+export const SIGNALR_HUB_URL = import.meta.env.MODE === 'development'
   ? import.meta.env.VITE_APP_SIGNALR_HUB_URL_DEVELOPMENT
   : import.meta.env.VITE_APP_SIGNALR_HUB_URL;
 
-export const API_BASE_URL = import.meta.env.DEV
+export const API_BASE_URL = import.meta.env.MODE === 'development'
   ? import.meta.env.VITE_APP_API_BASE_URL_DEVELOPMENT
   : import.meta.env.VITE_APP_API_BASE_URL;
