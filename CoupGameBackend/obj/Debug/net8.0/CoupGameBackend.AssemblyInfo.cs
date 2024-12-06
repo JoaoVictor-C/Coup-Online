ef5223f2d88eb8516ed6ee37cbfd2400da064513
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2024-12-06 14:29:16 -0300

    Some updates

--- CoupGameBackend/obj/Debug/net8.0/CoupGameBackend.AssemblyInfo.cs
+++ CoupGameBackend/obj/Debug/net8.0/CoupGameBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoupGameBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a713af414343a83f86fcb36c2e6efb85e0dd9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8dfa4100c31e9f2ba769175559e10dba691532")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoupGameBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoupGameBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
